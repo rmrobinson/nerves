6a2fa8b3c84c114fb431b3a657081de84224759b
Robert Robinson <r@faltung.ca>
2019-01-11 20:32:23 -0500

    Update terminal dashboard to properly save state to devices

--- services/ui/tboard/widget/devices.go
+++ services/ui/tboard/widget/devices.go
@@ -1,8 +1,11 @@
 package widget
 
 import (
+	"context"
+
 	"github.com/rivo/tview"
 	"github.com/rmrobinson/nerves/services/domotics"
+	"go.uber.org/zap"
 )
 
 // Devices is a widget to display an editable list of devices with their details available.
@@ -12,18 +15,23 @@ type Devices struct {
 	app  *tview.Application
 	next tview.Primitive
 
+	logger *zap.Logger
+
 	deviceList   *tview.List
 	deviceDetail *DeviceDetail
 
-	devices []*domotics.Device
+	devicesClient domotics.DeviceServiceClient
+	devices       []*domotics.Device
 }
 
 // NewDevices creates a new instance of this widget with the supplied set of devices for management.
-func NewDevices(app *tview.Application, devices []*domotics.Device) *Devices {
+func NewDevices(app *tview.Application, logger *zap.Logger, client domotics.DeviceServiceClient) *Devices {
 	d := &Devices{
-		Flex:    tview.NewFlex(),
-		app:     app,
-		devices: devices,
+		Flex:          tview.NewFlex(),
+		app:           app,
+		logger:        logger,
+		devices:       []*domotics.Device{},
+		devicesClient: client,
 	}
 
 	d.deviceList = tview.NewList().
@@ -31,7 +39,7 @@ func NewDevices(app *tview.Application, devices []*domotics.Device) *Devices {
 		SetSelectedFunc(d.onListEntryEntered).
 		SetDoneFunc(d.onListDone)
 
-	d.deviceDetail = NewDeviceDetail(app, d.deviceList)
+	d.deviceDetail = NewDeviceDetail(app, d.logger, d.deviceList)
 
 	d.SetBorder(true).
 		SetTitle("Devices").
@@ -41,6 +49,16 @@ func NewDevices(app *tview.Application, devices []*domotics.Device) *Devices {
 		AddItem(d.deviceList, 0, 1, true).
 		AddItem(d.deviceDetail, 28, 1, true)
 
+	listDevicesResp, err := d.devicesClient.ListDevices(context.Background(), &domotics.ListDevicesRequest{})
+	if err != nil {
+		logger.Warn("unable to retrieve devices",
+			zap.Error(err),
+		)
+		listDevicesResp = &domotics.ListDevicesResponse{}
+	}
+
+	d.devices = listDevicesResp.Devices
+
 	for _, device := range d.devices {
 		d.deviceList.AddItem(device.Config.Name, device.Config.Description, 0, nil)
 	}
@@ -49,7 +67,7 @@ func NewDevices(app *tview.Application, devices []*domotics.Device) *Devices {
 }
 
 func (d *Devices) onListEntrySelected(idx int, mainText string, secondaryText string, shortcut rune) {
-	d.deviceDetail.Refresh(d.devices[idx])
+	d.deviceDetail.Refresh(d.devicesClient, d.devices[idx])
 }
 
 func (d *Devices) onListEntryEntered(idx int, mainText string, secondaryText string, shortcut rune) {
