62a495e41a5c6d99b1d43264788991460733b922
Robert Robinson <r@faltung.ca>
2019-01-25 21:56:09 -0500

    Add GTFS realtime monitoring

--- services/ui/tboard/widget/transit.go
+++ services/ui/tboard/widget/transit.go
@@ -8,23 +8,28 @@ import (
 type transitRecord struct {
 	*tview.Flex
 
-	routeText       *tview.TextView
-	arrivalTimeText *tview.TextView
+	routeText            *tview.TextView
+	scheduledArrivalTime *tview.TextView
+	estimatedArrivalTime *tview.TextView
 }
 
 func newTransitRecord() *transitRecord {
 	tr := &transitRecord{
-		Flex:            tview.NewFlex(),
-		routeText:       tview.NewTextView(),
-		arrivalTimeText: tview.NewTextView(),
+		Flex:                 tview.NewFlex(),
+		routeText:            tview.NewTextView(),
+		scheduledArrivalTime: tview.NewTextView(),
+		estimatedArrivalTime: tview.NewTextView(),
 	}
 
 	tr.routeText.SetTextAlign(tview.AlignLeft)
-	tr.arrivalTimeText.SetTextAlign(tview.AlignRight)
+	tr.scheduledArrivalTime.SetTextAlign(tview.AlignLeft)
+	tr.estimatedArrivalTime.SetTextAlign(tview.AlignRight)
 
-	tr.SetDirection(tview.FlexColumn).
+	tr.SetDirection(tview.FlexRow).
 		AddItem(tr.routeText, 0, 1, false).
-		AddItem(tr.arrivalTimeText, 9, 1, false)
+		AddItem(tview.NewFlex().SetDirection(tview.FlexColumn).
+			AddItem(tr.scheduledArrivalTime, 0, 1, false).
+			AddItem(tr.estimatedArrivalTime, 0, 1, false), 0, 1, false)
 
 	return tr
 }
@@ -53,7 +58,7 @@ func NewTransit(app *tview.Application, rowCount int) *Transit {
 	wf.SetDirection(tview.FlexRow)
 	for i := 0; i < rowCount; i++ {
 		wf.records = append(wf.records, newTransitRecord())
-		wf.AddItem(wf.records[i], 1, 1, false)
+		wf.AddItem(wf.records[i], 2, 1, false)
 	}
 
 	return wf
@@ -66,14 +71,20 @@ func (wf *Transit) Refresh(stop *transit.Stop, records []*transit.Arrival) {
 		for i := 0; i < len(wf.records); i++ {
 			if i >= len(records) {
 				wf.records[i].routeText.Clear()
-				wf.records[i].arrivalTimeText.Clear()
+				wf.records[i].scheduledArrivalTime.Clear()
+				wf.records[i].estimatedArrivalTime.Clear()
 				continue
 			}
 
 			record := records[i]
 
 			wf.records[i].routeText.SetText(record.RouteId + " " + record.Headsign)
-			wf.records[i].arrivalTimeText.SetText(record.ScheduledArrivalTime)
+			wf.records[i].scheduledArrivalTime.SetText("S: " + record.ScheduledArrivalTime)
+			if len(record.EstimatedArrivalTime) > 0 {
+				wf.records[i].estimatedArrivalTime.SetText("E: " + record.EstimatedArrivalTime)
+			} else {
+				wf.records[i].estimatedArrivalTime.SetText("E: ??:??:??")
+			}
 		}
 	})
 }
