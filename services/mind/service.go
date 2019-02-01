8a9d72985bda9ebf7b063c663a9d2af9d211c26e
Robert Robinson <r@faltung.ca>
2019-01-31 20:58:33 -0500

    Initial slackbot
    * connects to the slack RTM API and receives messages

---
+++ services/mind/service.go
@@ -0,0 +1,34 @@
+package mind
+
+import (
+	"context"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"go.uber.org/zap"
+)
+
+type Service struct {
+	logger *zap.Logger
+}
+
+func NewService(logger *zap.Logger) *Service{
+	return &Service{
+		logger: logger,
+	}
+}
+
+func (s *Service) RegisterUser(context.Context, *RegisterUserRequest) (*User, error) {
+	return nil, nil
+}
+
+func (s *Service) SendStatement(ctx context.Context, req *SendStatementRequest) (*empty.Empty, error) {
+	s.logger.Debug("Received statement",
+		zap.String("name", req.Name),
+		zap.String("message", string(req.Statement.Content)),
+	)
+	return nil, nil
+}
+
+func (s *Service) ReceiveStatements(*ReceiveStatementsRequest, MessageService_ReceiveStatementsServer) error {
+	return nil
+}
