000c4f415e69d8230cb7a5b2009205b90e641219
Robert Robinson <r@faltung.ca>
2019-02-23 20:59:51 -0500

    Refactor news handling

--- services/news/api.go
+++ services/news/api.go
@@ -3,7 +3,10 @@ package news
 import (
 	"context"
 	"sort"
+	"time"
 
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/ptypes"
 	"go.uber.org/zap"
 )
 
@@ -11,16 +14,28 @@ import (
 type API struct {
 	logger *zap.Logger
 
-	cbcf *CBCFeed
-	bbcf *BBCFeed
+	articles map[string]*Article
 }
 
 // NewAPI creates a new instance of the API struct.
-func NewAPI(logger *zap.Logger, cbcf *CBCFeed, bbcf *BBCFeed) *API {
+func NewAPI(logger *zap.Logger) *API {
 	return &API{
-		logger: logger,
-		cbcf:   cbcf,
-		bbcf:   bbcf,
+		logger:   logger,
+		articles: map[string]*Article{},
+	}
+}
+
+func (a *API) refreshArticles(articles []*Article) {
+	for _, article := range articles {
+		if existingArticle, ok := a.articles[article.Name]; ok {
+			if !proto.Equal(existingArticle, article) && article.UpdatedAfter(existingArticle) {
+				a.articles[article.Name] = article
+			}
+
+			continue
+		}
+
+		a.articles[article.Name] = article
 	}
 }
 
@@ -32,12 +47,33 @@ func (a *API) StreamNewsUpdates(*StreamNewsUpdatesRequest, NewsService_StreamNew
 // ListArticles returns a collection of articles to the requester.
 func (a *API) ListArticles(ctx context.Context, req *ListArticlesRequest) (*ListArticlesResponse, error) {
 	ret := &ListArticlesResponse{}
-	ret.Articles = append(ret.Articles, a.cbcf.articles...)
-	ret.Articles = append(ret.Articles, a.bbcf.articles...)
+
+	for _, article := range a.articles {
+		ret.Articles = append(ret.Articles, article)
+	}
 
 	sort.Slice(ret.Articles, func(i, j int) bool {
 		return ret.Articles[i].CreateTime.Seconds > ret.Articles[j].CreateTime.Seconds
 	})
 
 	return ret, nil
+}
+
+// UpdatedAfter is used to determine if this article was updated after the supplied article.
+func (a *Article) UpdatedAfter(o *Article) bool {
+	var aUpdated time.Time
+	if a.UpdateTime != nil {
+		aUpdated, _ = ptypes.Timestamp(a.UpdateTime)
+	} else {
+		aUpdated, _ = ptypes.Timestamp(a.CreateTime)
+	}
+
+	var oUpdated time.Time
+	if o.UpdateTime != nil {
+		oUpdated, _ = ptypes.Timestamp(o.UpdateTime)
+	} else {
+		oUpdated, _ = ptypes.Timestamp(o.CreateTime)
+	}
+
+	return aUpdated.After(oUpdated)
 }
