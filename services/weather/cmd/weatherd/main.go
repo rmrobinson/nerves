c47d18aaaa486e4a085ccebcbfc2838ee51654b2
Robert Robinson <r@faltung.ca>
2019-02-28 21:44:51 -0500

    Refactor weather setup to allow multiple providers to register stations

--- services/weather/cmd/weatherd/main.go
+++ services/weather/cmd/weatherd/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/rmrobinson/nerves/services/weather"
 	"github.com/rmrobinson/nerves/services/weather/envcan"
+	"github.com/rmrobinson/nerves/services/weather/noaa"
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
@@ -15,15 +16,22 @@ func main() {
 	viper.SetEnvPrefix("NVS")
 	viper.BindEnv("ENVCAN_MAP")
 
-	logger, _ := zap.NewDevelopment()
-	ecsvc, err := envcan.NewService(logger, viper.GetString("ENVCAN_MAP"))
+	logger, err := zap.NewDevelopment()
 	if err != nil {
-		logger.Fatal("error creating feed",
+		panic(err)
+	}
+
+	api := weather.NewAPI(logger)
+
+	_, err = envcan.NewService(logger, api, viper.GetString("ENVCAN_MAP"))
+	if err != nil {
+		logger.Fatal("error creating envcan service",
 			zap.Error(err),
 		)
 	}
 
-	//go ecsvc.Run(context.Background())
+	sfStation := noaa.NewStation(logger, "https://api.weather.gov/gridpoints/MTR/88,126", "San Francisco", 37.7749, -122.4194)
+	api.RegisterStation(sfStation, 37.7749, -122.4194)
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 10101))
 	if err != nil {
@@ -33,7 +41,7 @@ func main() {
 	}
 
 	grpcServer := grpc.NewServer()
-	weather.RegisterWeatherServiceServer(grpcServer, weather.NewAPI(ecsvc))
+	weather.RegisterWeatherServiceServer(grpcServer, api)
 	err = grpcServer.Serve(lis)
 	if err != nil {
 		logger.Fatal("failed to serve",
