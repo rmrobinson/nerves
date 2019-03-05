a8a0e4bf2e2eda73ab17eed3431bab9f9982e31b
Robert Robinson <r@faltung.ca>
2019-03-04 20:51:44 -0800

    Policy condition execution

--- services/policy/policy.go
+++ services/policy/policy.go
@@ -1,5 +1,45 @@
 package policy
 
-func (p *Policy) execute(state *State) {
-	// TODO: evaluate conditions then apply actions.
+import (
+	"go.uber.org/zap"
+)
+
+func (p *Policy) execute(logger *zap.Logger, state *State) {
+	if !p.Condition.triggered(state) {
+		logger.Debug("policy conditions not met",
+			zap.String("name", p.Name),
+		)
+		return
+	}
+	// TODO: apply actions.
+}
+
+func (c *Condition) triggered(state *State) bool {
+	if c.Set != nil {
+		anyTriggered := false
+		allTriggered := true
+		for _, condition := range c.Set.Conditions {
+			triggered := condition.triggered(state)
+
+			if triggered && c.Set.Operator == Condition_Set_OR {
+				anyTriggered = true
+				break
+			} else if !triggered && c.Set.Operator == Condition_Set_AND {
+				allTriggered = false
+				break
+			}
+		}
+
+		if c.Set.Operator == Condition_Set_OR && anyTriggered {
+			return true
+		} else if c.Set.Operator == Condition_Set_AND && allTriggered {
+			return true
+		}
+
+		return false
+	}
+
+	// TODO: add other conditions
+
+	return false
 }
